fac942713a814a53cad7a3be3c9501f4c229c179
adnanioricce <adnangonzagadevio@gmail.com>
2024-09-06 18:20:48 -0300

    [Api/Products] - Renomeando CreateProducts para CreateOrUpdateProducts

--- Mobit.Web/Services/IProductService.cs
+++ Mobit.Web/Services/IProductService.cs
@@ -6,7 +6,7 @@ public interface IProductService
 	Task<IEnumerable<ProductDto>> GetProductsAsync(Pagination pagination);
 	Task<ProductDto?> GetProductByIdAsync(int productId);
 	Task<ProductDto?> CreateProductAsync(ProductDto product);
-	Task CreateProductsAsync(IEnumerable<Product> products);
+	Task CreateOrUpdateProductsAsync(IEnumerable<Product> products);
 	Task EditProductAsync(int productId,ProductDto product);
 	Task DeleteProductAsync(int productId);
 	
